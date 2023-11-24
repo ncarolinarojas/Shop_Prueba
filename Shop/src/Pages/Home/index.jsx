187f850a3aff06c783669ed457b0fa6720e3b82f
ncarolinarojas <nataliarojasco22@gmail.com>
2023-11-23 22:30:17 -0500

    Renderizado de componentes

--- Shop/src/Pages/Home/index.jsx
+++ Shop/src/Pages/Home/index.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react'
 import Layout from "../../Components/Layout"
+import Card from "../../Components/Card"
 
 function Home() {
+
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  console.log(products)
+
   return (
-  <Layout> Home </Layout>
+    <Layout>
+      Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          products?.map(product => (
+            <Card key={product.id} data={product} />
+          ))
+        }
+      </div>
+    </Layout>
   )
 }
 
